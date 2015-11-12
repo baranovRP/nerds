fef8f7644756d433a38ea5d578f23bc631666be8
Roman Baranov <baranov.r.p@gmail.com>
2015-11-12 22:04:22 +0100

    #nerds: set correct font-weight, font-family; add fonts settings directly to used tag; fix broken selectors; tune margins/paddings; format css according to styleguide; join js in one file;

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* feedback-form */
 var link = document.querySelector(".contacts-email");
 var popup = document.querySelector(".modal-content");
 var close = document.querySelector(".modal-content-close");
@@ -119,3 +120,37 @@ window.addEventListener("keydown", function (event) {
     }
   }
 });
+
+
+/* contact-map */
+function initMap() {
+
+  var map = new google.maps.Map(document.getElementById("map-container"), {
+    zoom: 17,
+    scrollwheel: false,
+    center: {
+      lat: 59.939098,
+      lng: 30.319898
+    }
+
+  });
+
+  var contactLatLng = {
+    lat: 59.938794,
+    lng: 30.323083
+  };
+
+  var contactLogo = {
+    url: "img/contacts-map-logo.png",
+    size: new google.maps.Size(231, 190),
+    anchor: new google.maps.Point(59, 152)
+  };
+
+  var marker = new google.maps.Marker({
+    position: contactLatLng,
+    icon: contactLogo,
+    draggable: true,
+    map: map
+  });
+
+}
